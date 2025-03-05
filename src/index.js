c305ece443360df7911d7be73278701b7b33d901
gruposk-3021 <gruposkcapital@gmail.com>
2025-03-05 14:02:16 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('🚀 Deploy Railway Funcionando!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
 
